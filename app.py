7d268f13f7875a68469e3508d525d0d280b2bcd7
Mayank-iitp <151143725+Mayank-iitp@users.noreply.github.com>
2024-08-15 14:44:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ embeddings=HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 
 
 ## set up Streamlit 
-st.title("Conversational RAG With PDF uplaods and chat history")
+st.title("InsightBot")
 st.write("Upload Pdf's and chat with their content")
 
 ## Input the Groq API Key
